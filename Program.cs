af5c26ae007402f35a6e1088a173a7e9924f2ed3
Romeo <tf00117345@gmail.com>
2025-02-05 20:20:32 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,120 @@
+using System.Text;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// 設定 Serilog 作為日誌處理器
+Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration).CreateLogger();
+
+// 使用 Serilog 作為日誌處理器
+builder.Host.UseSerilog();
+
+// 獲取應用配置
+var configuration = builder.Configuration;
+
+// 配置 Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Title = "Your API",
+            Version = "v1",
+            Description = "Your API Description"
+        }
+    );
+
+    // 添加 JWT 認證配置
+    c.AddSecurityDefinition(
+        "Bearer",
+        new OpenApiSecurityScheme
+        {
+            Description =
+                "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        }
+    );
+
+    c.AddSecurityRequirement(
+        new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        }
+    );
+});
+
+// 配置 JWT 認證
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = configuration["Jwt:Issuer"],
+            ValidAudience = configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(configuration["Jwt:Key"])
+            )
+        };
+    });
+
+// 配置 CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        }
+    );
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// 使用 CORS
+app.UseCors("AllowAll");
+
+// 添加認證中間件
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# WoundCareApi
+# Getting Started with EF
+
+## 安裝工具
+
+#### dotnet ef 可以安裝為全域或本機工具。 大部分開發人員偏好使用下列命令安裝 dotnet ef 為全域工具：
+
+`dotnet tool install --global dotnet-ef`
+
+#### 使用下列命令更新工具：
+
+`dotnet tool update --global dotnet-ef`
+
+#### 確認安裝
+
+`dotnet ef`
+
+## 建立 Table
+
+### dotnet ef dbcontext scaffold
+
+#### 為 資料庫產生 和 實體類型的程式碼 DbContext 。 為了讓此命令產生實體類型，資料庫資料表必須有主鍵。
+
+`dotnet ef dbcontext scaffold "Server=localhost;Database=ReportServer;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer --context ApplicationDbContext --context-dir Persistence --output-dir Core/Domain --use-database-names --force`
+
+## 參考
+
+See the [Link](https://docs.microsoft.com/zh-tw/ef/core/cli/dotnet) for more information.
+See the [Entity framework tutorial](https://www.entityframeworktutorial.net/code-first/setup-entity-framework-code-first-environment.aspx) for more information.
